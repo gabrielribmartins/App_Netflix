37cc71a432b56bf9b174e689a87bfeff1e461bc6
Gabriel Martins <gabrielribmartins@gmail.comn>
2023-09-26 21:58:44 -0300

    Commit inicial

---
+++ app/src/main/java/com/gabrielmartins/app_netflix/SignUpWelcomeActivity.kt
@@ -0,0 +1,11 @@
+package com.gabrielmartins.app_netflix
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpWelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up_welcome)
+    }
+}
